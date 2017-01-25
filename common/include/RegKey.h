9faee3b28e01b635977ced8f320e0fae011e0ead
Rafał H <rafalh92@outlook.com>
2017-01-25 21:22:22 +0100

    Fix error messages for RegKey class.

--- common/include/RegKey.h
+++ common/include/RegKey.h
@@ -9,8 +9,9 @@ class RegKey
 public:
     RegKey(HKEY parentKey, const char *subKey, REGSAM accessRights)
     {
-        if (RegCreateKeyExA(parentKey, subKey, NULL, NULL, 0, accessRights, NULL, &m_key, NULL) != ERROR_SUCCESS)
-            THROW_EXCEPTION("RegCreateKeyExA failed: win32 error %lu", GetLastError());
+        LONG error = RegCreateKeyExA(parentKey, subKey, 0, NULL, 0, accessRights, NULL, &m_key, NULL);
+        if (error != ERROR_SUCCESS)
+            THROW_EXCEPTION("RegCreateKeyExA failed: win32 error %lu", error);
     }
 
     RegKey(RegKey parentKey, const char *subKey, REGSAM accessRights) :
@@ -24,7 +25,8 @@ public:
     bool readValue(const char *name, unsigned *value)
     {
         DWORD tempValue, type, cb = sizeof(tempValue);
-        if (RegQueryValueEx(m_key, name, NULL, &type, (LPBYTE)&tempValue, &cb) != ERROR_SUCCESS)
+        LONG error = RegQueryValueEx(m_key, name, NULL, &type, (LPBYTE)&tempValue, &cb);
+        if (error != ERROR_SUCCESS)
             return false;
         if (type != REG_DWORD)
             return false;
@@ -35,14 +37,16 @@ public:
     bool readValue(const char *name, std::string *value)
     {
         DWORD type, cb = 0;
-        if (RegQueryValueEx(m_key, name, NULL, &type, NULL, &cb) != ERROR_SUCCESS)
+        LONG error = RegQueryValueEx(m_key, name, NULL, &type, NULL, &cb);
+        if (error != ERROR_SUCCESS)
             return false;
         if (type != REG_SZ && type != REG_EXPAND_SZ)
             return false;
 
         value->clear();
         value->resize(cb + 1);
-        if (!RegQueryValueEx(m_key, name, NULL, &type, (LPBYTE)value->data(), &cb))
+        error = RegQueryValueEx(m_key, name, NULL, &type, (LPBYTE)value->data(), &cb);
+        if (error != ERROR_SUCCESS)
             return false;
         ((LPBYTE)value->data())[cb] = 0;
         return true;
@@ -60,14 +64,16 @@ public:
     void writeValue(const char *name, unsigned value)
     {
         DWORD temp = value;
-        if (RegSetValueExA(m_key, name, NULL, REG_DWORD, (BYTE*)&temp, sizeof(temp)) != ERROR_SUCCESS)
-            THROW_EXCEPTION("RegSetValueExA failed: win32 error %lu", GetLastError());
+        LONG error = RegSetValueExA(m_key, name, NULL, REG_DWORD, (BYTE*)&temp, sizeof(temp));
+        if (error != ERROR_SUCCESS)
+            THROW_EXCEPTION("RegSetValueExA failed: win32 error %lu", error);
     }
 
     void writeValue(const char *name, const std::string &value)
     {
-        if (RegSetValueExA(m_key, name, NULL, REG_SZ, (BYTE*)value.c_str(), value.size() + 1) != ERROR_SUCCESS)
-            THROW_EXCEPTION("RegSetValueExA failed: win32 error %lu", GetLastError());
+        LONG error = RegSetValueExA(m_key, name, NULL, REG_SZ, (BYTE*)value.c_str(), value.size() + 1);
+        if (error != ERROR_SUCCESS)
+            THROW_EXCEPTION("RegSetValueExA failed: win32 error %lu", error);
     }
 
     void writeValue(const char *name, bool value)
