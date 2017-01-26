be74d9732e1d5d9ff5a237197f5787ea4d155e09
Rafał H <rafalh92@outlook.com>
2017-01-26 22:34:38 +0100

    Change version to 1.0.1-dev.

--- common/include/version.h
+++ common/include/version.h
@@ -3,6 +3,6 @@
 #define PRODUCT_NAME          "Dash Faction"
 #define VERSION_MAJOR         1
 #define VERSION_MINOR         0
-#define VERSION_PATCH         0
-#define VERSION_STR           "1.0.0"
+#define VERSION_PATCH         1
+#define VERSION_STR           "1.0.1-dev"
 #define PRODUCT_NAME_VERSION  PRODUCT_NAME " " VERSION_STR
