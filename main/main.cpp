579f0941c14be885e66945abae8901236b93f963
Rafał H <rafalh92@outlook.com>
2017-02-15 22:29:57 +0100

    Factor out parts of main.cpp into misc.cpp (some not important stuff).

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,5 @@
 #include "stdafx.h"
 #include "exports.h"
-#include "version.h"
 #include "crashdump.h"
 #include "autodl.h"
 #include "utils.h"
@@ -19,7 +18,7 @@
 #include "network.h"
 #include "test.h"
 #include "high_fps.h"
-#include "HookableFunPtr.h"
+#include "misc.h"
 #include "GameConfig.h"
 
 #include <log/FileAppender.h>
@@ -31,12 +30,9 @@ using namespace rf;
 GameConfig g_gameConfig;
 HMODULE g_hModule;
 auto RenderHitScreen_Hook = makeFunHook(RenderHitScreen);
-auto GetVersionStr_Hook = makeFunHook(GetVersionStr);
 auto PlayerCreate_Hook = makeFunHook(PlayerCreate);
 auto PlayerDestroy_Hook = makeFunHook(PlayerDestroy);
 auto KeyGetFromFifo_Hook = makeFunHook(KeyGetFromFifo);
-int g_VersionLabelX, g_VersionLabelWidth, g_VersionLabelHeight;
-static const char g_szVersionInMenu[] = PRODUCT_NAME_VERSION;
 
 static void ProcessWaitingMessages()
 {
@@ -53,6 +49,20 @@ static void ProcessWaitingMessages()
     }
 }
 
+CPlayer *FindPlayer(const char *pszName)
+{
+    CPlayer *pPlayer = *g_ppPlayersList;
+    while (pPlayer)
+    {
+        if (stristr(pPlayer->strName.psz, pszName))
+            return pPlayer;
+        pPlayer = pPlayer->pNext;
+        if (pPlayer == *g_ppPlayersList)
+            break;
+    }
+    return NULL;
+}
+
 static void DcUpdateHook(BOOL bServer)
 {
     // Draw on top (after scene)
@@ -64,33 +74,6 @@ static void DcUpdateHook(BOOL bServer)
 #ifdef LEVELS_AUTODOWNLOADER
     RenderDownloadProgress();
 #endif
-}
-
-NAKED void VersionLabelPushArgs_0044343A()
-{
-    _asm
-    {
-        // Default: 330, 340, 120, 15
-        push    g_VersionLabelHeight
-        push    g_VersionLabelWidth
-        push    340
-        push    g_VersionLabelX
-        mov eax, 0x00443448
-        jmp eax
-    }
-}
-
-static void GetVersionStr_New(const char **ppszVersion, const char **a2)
-{
-    if (ppszVersion)
-        *ppszVersion = g_szVersionInMenu;
-    if (a2)
-        *a2 = "";
-    GrGetTextWidth(&g_VersionLabelWidth, &g_VersionLabelHeight, g_szVersionInMenu, -1, *g_pMediumFontId);
-
-    g_VersionLabelX = 430 - g_VersionLabelWidth;
-    g_VersionLabelWidth = g_VersionLabelWidth + 5;
-    g_VersionLabelHeight = g_VersionLabelHeight + 2;
 }
 
 static void InitGameHook(void)
@@ -136,20 +119,6 @@ int KeyGetFromFifo_New()
     return KeyGetFromFifo_Hook.callTrampoline();
 }
 
-CPlayer *FindPlayer(const char *pszName)
-{
-    CPlayer *pPlayer = *g_ppPlayersList;
-    while(pPlayer)
-    {
-        if(stristr(pPlayer->strName.psz, pszName))
-            return pPlayer;
-        pPlayer = pPlayer->pNext;
-        if(pPlayer == *g_ppPlayersList)
-            break;
-    }
-    return NULL;
-}
-
 static void RenderHitScreen_New(CPlayer *pPlayer)
 {
     RenderHitScreen_Hook.callTrampoline(pPlayer);
@@ -174,29 +143,6 @@ static void PlayerDestroy_New(CPlayer *pPlayer)
 {
     SpectateModeOnDestroyPlayer(pPlayer);
     PlayerDestroy_Hook.callTrampoline(pPlayer);
-}
-
-NAKED void CrashFix_0055CE48()
-{
-    _asm
-    {
-        shl   edi, 5
-        lea   edx, [esp + 0x38 - 0x28]
-        mov   eax, [eax + edi]
-        test  eax, eax // check if pD3DTexture is NULL
-        jz    CrashFix0055CE59_label1
-        //jmp CrashFix0055CE59_label1
-        push  0
-        push  0
-        push  edx
-        push  0
-        push  eax
-        mov   ecx, 0x0055CE59
-        jmp   ecx
-    CrashFix0055CE59_label1:
-        mov   ecx, 0x0055CF23 // fail gr_lock
-        jmp   ecx
-    }
 }
 
 #ifndef NDEBUG
@@ -271,85 +217,18 @@ extern "C" DWORD DLL_EXPORT Init(void *pUnused)
     WriteMemUInt8(0x00524C48, ASM_LONG_CALL_REL);
     WriteMemUInt32(0x00524C49, 0x00524E40 - (0x00524C48 + 0x5)); // CreateMainWindow
     KeyGetFromFifo_Hook.hook(KeyGetFromFifo_New);
-
-    /* Console init string */
-    WriteMemPtr(0x004B2534, "-- " PRODUCT_NAME " Initializing --\n");
-
-    /* Version in Main Menu */
-    WriteMemUInt8(0x0044343A, ASM_LONG_JMP_REL);
-    WriteMemInt32(0x0044343A + 1, (uintptr_t)VersionLabelPushArgs_0044343A - (0x0044343A + 0x5));
-    GetVersionStr_Hook.hook(GetVersionStr_New);
     
-    /* Window title (client and server) */
-    WriteMemPtr(0x004B2790, PRODUCT_NAME);
-    WriteMemPtr(0x004B27A4, PRODUCT_NAME);
-    
-    /* Console background color */
-    WriteMemUInt32(0x005098D1, CONSOLE_BG_A); // Alpha
-    WriteMemUInt8(0x005098D6, CONSOLE_BG_B); // Blue
-    WriteMemUInt8(0x005098D8, CONSOLE_BG_G); // Green
-    WriteMemUInt8(0x005098DA, CONSOLE_BG_R); // Red
-
-#ifdef NO_CD_FIX
-    /* No-CD fix */
-    WriteMemUInt8(0x004B31B6, ASM_SHORT_JMP_REL);
-#endif /* NO_CD_FIX */
-
-#ifdef NO_INTRO
-    /* Disable thqlogo.bik */
-    if (g_gameConfig.fastStart)
-    {
-        WriteMemUInt8(0x004B208A, ASM_SHORT_JMP_REL);
-        WriteMemUInt8(0x004B24FD, ASM_SHORT_JMP_REL);
-    }
-#endif /* NO_INTRO */
-    
-    /* Sound loop fix */
-    WriteMemUInt8(0x00505D08, 0x00505D5B - (0x00505D07 + 0x2));
-    
-    /* DrawConsoleAndProcssKbdFifo hook */
+    /* Debug Console update hook */
     WriteMemInt32(0x004B2DD3 + 1, (uintptr_t)DcUpdateHook - (0x004B2DD3 + 0x5));
 
-    /* CleanupGame and InitGame hooks */
+    /* General game hooks */
     WriteMemInt32(0x004B27CD + 1, (uintptr_t)InitGameHook - (0x004B27CD + 0x5));
     WriteMemInt32(0x004B2821 + 1, (uintptr_t)CleanupGameHook - (0x004B2821 + 0x5));
     WriteMemInt32(0x004B2818 + 1, (uintptr_t)RunGameHook - (0x004B2818 + 0x5));
-    
-    /* Set initial FPS limit */
-    WriteMemFloat(0x005094CA, 1.0f / g_gameConfig.maxFps);
-    
-    /* Crash-fix... (probably argument for function is invalid); Page Heap is needed */
-    WriteMemUInt32(0x0056A28C + 1, 0);
-
-    /* Crash-fix in case texture has not been created (this happens if GrReadBackbuffer fails) */
-    WriteMemUInt8(0x0055CE47, ASM_LONG_JMP_REL);
-    WriteMemInt32(0x0055CE47 + 1, (uintptr_t)CrashFix_0055CE48 - (0x0055CE47 + 0x5));
-    
-    // Dont overwrite player name and prefered weapons when loading saved game
-    WriteMemUInt8(0x004B4D99, ASM_NOP, 0x004B4DA5 - 0x004B4D99);
-    WriteMemUInt8(0x004B4E0A, ASM_NOP, 0x004B4E22 - 0x004B4E0A);
-
-    // Dont filter levels for DM and TeamDM
-    WriteMemUInt8(0x005995B0, 0);
-    WriteMemUInt8(0x005995B8, 0);
 
     RenderHitScreen_Hook.hook(RenderHitScreen_New);
     PlayerCreate_Hook.hook(PlayerCreate_New);
     PlayerDestroy_Hook.hook(PlayerDestroy_New);
-
-#if DIRECTINPUT_SUPPORT
-    *g_pbDirectInputDisabled = 0;
-#endif
-
-#if 1
-    // Buffer overflows in RflReadStaticGeometry
-    // Note: Buffer size is 1024 but opcode allows only 1 byte size
-    //       What is more important BmLoad copies texture name to 32 bytes long buffers
-    WriteMemInt8(0x004ED612 + 1, 32);
-    WriteMemInt8(0x004ED66E + 1, 32);
-    WriteMemInt8(0x004ED72E + 1, 32);
-    WriteMemInt8(0x004EDB02 + 1, 32);
-#endif
 
     /* Init modules */
     GraphicsInit();
@@ -368,6 +247,7 @@ extern "C" DWORD DLL_EXPORT Init(void *pUnused)
     SpectateModeInit();
     CommandsInit();
     HighFpsInit();
+    MiscInit();
 #ifndef NDEBUG
     TestInit();
 #endif
---
+++ main/misc.cpp
@@ -0,0 +1,132 @@
+#include "stdafx.h"
+#include "misc.h"
+#include "rf.h"
+#include "version.h"
+#include "utils.h"
+#include "main.h"
+
+using namespace rf;
+
+auto GetVersionStr_Hook = makeFunHook(GetVersionStr);
+int g_VersionLabelX, g_VersionLabelWidth, g_VersionLabelHeight;
+static const char g_szVersionInMenu[] = PRODUCT_NAME_VERSION;
+
+NAKED void VersionLabelPushArgs_0044343A()
+{
+    _asm
+    {
+        // Default: 330, 340, 120, 15
+        push    g_VersionLabelHeight
+        push    g_VersionLabelWidth
+        push    340
+        push    g_VersionLabelX
+        mov eax, 0x00443448
+        jmp eax
+    }
+}
+
+static void GetVersionStr_New(const char **ppszVersion, const char **a2)
+{
+    if (ppszVersion)
+        *ppszVersion = g_szVersionInMenu;
+    if (a2)
+        *a2 = "";
+    GrGetTextWidth(&g_VersionLabelWidth, &g_VersionLabelHeight, g_szVersionInMenu, -1, *g_pMediumFontId);
+
+    g_VersionLabelX = 430 - g_VersionLabelWidth;
+    g_VersionLabelWidth = g_VersionLabelWidth + 5;
+    g_VersionLabelHeight = g_VersionLabelHeight + 2;
+}
+
+NAKED void CrashFix_0055CE48()
+{
+    _asm
+    {
+        shl   edi, 5
+        lea   edx, [esp + 0x38 - 0x28]
+        mov   eax, [eax + edi]
+        test  eax, eax // check if pD3DTexture is NULL
+        jz    CrashFix0055CE59_label1
+        //jmp CrashFix0055CE59_label1
+        push  0
+        push  0
+        push  edx
+        push  0
+        push  eax
+        mov   ecx, 0x0055CE59
+        jmp   ecx
+        CrashFix0055CE59_label1 :
+        mov   ecx, 0x0055CF23 // fail gr_lock
+            jmp   ecx
+    }
+}
+
+void MiscInit()
+{
+    /* Console init string */
+    WriteMemPtr(0x004B2534, "-- " PRODUCT_NAME " Initializing --\n");
+
+    /* Version in Main Menu */
+    WriteMemUInt8(0x0044343A, ASM_LONG_JMP_REL);
+    WriteMemInt32(0x0044343A + 1, (uintptr_t)VersionLabelPushArgs_0044343A - (0x0044343A + 0x5));
+    GetVersionStr_Hook.hook(GetVersionStr_New);
+
+    /* Window title (client and server) */
+    WriteMemPtr(0x004B2790, PRODUCT_NAME);
+    WriteMemPtr(0x004B27A4, PRODUCT_NAME);
+
+    /* Console background color */
+    WriteMemUInt32(0x005098D1, CONSOLE_BG_A); // Alpha
+    WriteMemUInt8(0x005098D6, CONSOLE_BG_B); // Blue
+    WriteMemUInt8(0x005098D8, CONSOLE_BG_G); // Green
+    WriteMemUInt8(0x005098DA, CONSOLE_BG_R); // Red
+
+#ifdef NO_CD_FIX
+                                             /* No-CD fix */
+    WriteMemUInt8(0x004B31B6, ASM_SHORT_JMP_REL);
+#endif /* NO_CD_FIX */
+
+#ifdef NO_INTRO
+    /* Disable thqlogo.bik */
+    if (g_gameConfig.fastStart)
+    {
+        WriteMemUInt8(0x004B208A, ASM_SHORT_JMP_REL);
+        WriteMemUInt8(0x004B24FD, ASM_SHORT_JMP_REL);
+    }
+#endif /* NO_INTRO */
+
+    /* Sound loop fix */
+    WriteMemUInt8(0x00505D08, 0x00505D5B - (0x00505D07 + 0x2));
+
+    /* Set initial FPS limit */
+    WriteMemFloat(0x005094CA, 1.0f / g_gameConfig.maxFps);
+
+    /* Crash-fix... (probably argument for function is invalid); Page Heap is needed */
+    WriteMemUInt32(0x0056A28C + 1, 0);
+
+    /* Crash-fix in case texture has not been created (this happens if GrReadBackbuffer fails) */
+    WriteMemUInt8(0x0055CE47, ASM_LONG_JMP_REL);
+    WriteMemInt32(0x0055CE47 + 1, (uintptr_t)CrashFix_0055CE48 - (0x0055CE47 + 0x5));
+
+    // Dont overwrite player name and prefered weapons when loading saved game
+    WriteMemUInt8(0x004B4D99, ASM_NOP, 0x004B4DA5 - 0x004B4D99);
+    WriteMemUInt8(0x004B4E0A, ASM_NOP, 0x004B4E22 - 0x004B4E0A);
+
+    // Dont filter levels for DM and TeamDM
+    WriteMemUInt8(0x005995B0, 0);
+    WriteMemUInt8(0x005995B8, 0);
+
+#if DIRECTINPUT_SUPPORT
+    *g_pbDirectInputDisabled = 0;
+#endif
+
+#if 1
+    // Buffer overflows in RflReadStaticGeometry
+    // Note: Buffer size is 1024 but opcode allows only 1 byte size
+    //       What is more important BmLoad copies texture name to 32 bytes long buffers
+    WriteMemInt8(0x004ED612 + 1, 32);
+    WriteMemInt8(0x004ED66E + 1, 32);
+    WriteMemInt8(0x004ED72E + 1, 32);
+    WriteMemInt8(0x004EDB02 + 1, 32);
+#endif
+}
