97ba4abfc1306b67c4634e40a95cd6d9c7fee79c
Rafał H <rafalh92@outlook.com>
2017-05-04 23:30:08 +0200

    Change version to 1.2.0-alpha and update changelog.

--- common/include/version.h
+++ common/include/version.h
@@ -2,7 +2,7 @@
 
 #define PRODUCT_NAME          "Dash Faction"
 #define VERSION_MAJOR         1
-#define VERSION_MINOR         1
-#define VERSION_PATCH         1
-#define VERSION_STR           "1.1.1-dev"
+#define VERSION_MINOR         2
+#define VERSION_PATCH         0
+#define VERSION_STR           "1.2.0-alpha"
 #define PRODUCT_NAME_VERSION  PRODUCT_NAME " " VERSION_STR
