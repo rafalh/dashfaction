e2097f9da943d86274cb0fe22413dd84638d72f8
Rafał Harabień <rafalh92@outlook.com>
2018-12-16 19:41:24 +0100

    Change version to 1.2.2-dev

--- common/include/version.h
+++ common/include/version.h
@@ -3,6 +3,6 @@
 #define PRODUCT_NAME          "Dash Faction"
 #define VERSION_MAJOR         1
 #define VERSION_MINOR         2
-#define VERSION_PATCH         1
-#define VERSION_STR           "1.2.1"
+#define VERSION_PATCH         2
+#define VERSION_STR           "1.2.2-dev"
 #define PRODUCT_NAME_VERSION  PRODUCT_NAME " " VERSION_STR
