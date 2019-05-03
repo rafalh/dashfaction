056cabc2de0028dbffbbcae0a3e0413c242524eb
Rafał Harabień <rafalh92@outlook.com>
2019-05-03 15:38:41 +0200

    Deduplicate code for CodeInjection2

--- patch_common/include/patch_common/CodeInjection.h
+++ patch_common/include/patch_common/CodeInjection.h
@@ -5,16 +5,16 @@
 #include <log/Logger.h>
 #include <subhook.h>
 
-#define REG_UNION(letter)     \
-    union                     \
-    {                         \
-        int32_t e##letter##x; \
-        int16_t letter##x;    \
-        struct                \
-        {                     \
-            int8_t letter##l; \
-            int8_t letter##h; \
-        };                    \
+#define X86_GP_REG_UNION(letter) \
+    union                        \
+    {                            \
+        int32_t e##letter##x;    \
+        int16_t letter##x;       \
+        struct                   \
+        {                        \
+            int8_t letter##l;    \
+            int8_t letter##h;    \
+        };                       \
     }
 
 struct X86Regs
@@ -25,25 +25,27 @@ struct X86Regs
     int32_t esi;
     int32_t ebp;
     int32_t reserved; // unused esp
-    REG_UNION(b);
-    REG_UNION(d);
-    REG_UNION(c);
-    REG_UNION(a);
+    X86_GP_REG_UNION(b);
+    X86_GP_REG_UNION(d);
+    X86_GP_REG_UNION(c);
+    X86_GP_REG_UNION(a);
     // real esp
     int32_t esp;
     // return address
     uint32_t eip;
 };
 
-class CodeInjection
+#undef X86_GP_REG_UNION
+
+class BaseCodeInjection
 {
 private:
     uintptr_t m_addr;
-    void (*m_fun_ptr)(X86Regs& regs);
     subhook::Hook m_subhook;
 
 public:
-    CodeInjection(uintptr_t addr, void (*fun_ptr)(X86Regs& regs)) : m_addr(addr), m_fun_ptr(fun_ptr) {}
+    BaseCodeInjection(uintptr_t addr) : m_addr(addr) {}
+    virtual ~BaseCodeInjection() {}
 
     void Install()
     {
@@ -54,15 +56,15 @@ public:
         if (!trampoline)
             WARN("trampoline is null for 0x%X", m_addr);
 
-        AsmWritter(reinterpret_cast<uintptr_t>(wrapper))
+        AsmWritter asm_writter{reinterpret_cast<uintptr_t>(wrapper)};
+        asm_writter
             .push(reinterpret_cast<int32_t>(trampoline)) // Push default EIP = trampoline
             .push(asm_regs::esp) // push esp before PUSHA so it can be popped manually after POPA
             .pusha()             // push general registers
             .pushf()             // push EFLAGS
-            .add({true, {asm_regs::esp}, offsetof(X86Regs, esp)}, 4) // restore esp from before pushing the return address
-            .push(asm_regs::esp)              // push address of X86Regs struct (handler param)
-            .call(m_fun_ptr)                  // call handler
-            .add({false, {asm_regs::esp}}, 4) // free handler param
+            .add({true, {asm_regs::esp}, offsetof(X86Regs, esp)}, 4); // restore esp from before pushing the return address
+        EmitHandlerCall(asm_writter);         // call handler
+        asm_writter
             .add({true, {asm_regs::esp}, offsetof(X86Regs, esp)}, -4) // make esp value return address aware (again)
             .mov(asm_regs::eax, {true, {asm_regs::esp}, offsetof(X86Regs, eip)}) // read EIP from X86Regs
             .mov(asm_regs::ecx, {true, {asm_regs::esp}, offsetof(X86Regs, esp)}) // read esp from X86Regs
@@ -77,70 +79,52 @@ public:
     {
         m_addr = addr;
     }
+
+protected:
+    virtual void EmitHandlerCall(AsmWritter& asm_writter) = 0;
 };
 
-class BaseCodeInjection2
+class CodeInjection : public BaseCodeInjection
 {
 private:
-    uintptr_t m_addr;
-    void* m_fun_ptr;
-    void* m_this_ptr;
-    subhook::Hook m_subhook;
+    void (*m_fun_ptr)(X86Regs& regs);
 
 public:
-    BaseCodeInjection2(uintptr_t addr, void* fun_ptr, void* this_ptr) :
-        m_addr(addr), m_fun_ptr(fun_ptr), m_this_ptr(this_ptr)
-    {}
+    CodeInjection(uintptr_t addr, void (*fun_ptr)(X86Regs& regs)) :
+        BaseCodeInjection(addr), m_fun_ptr(fun_ptr) {}
 
-    virtual ~BaseCodeInjection2(){};
-
-    void Install()
+protected:
+    void EmitHandlerCall(AsmWritter& asm_writter) override
     {
-        void* wrapper = AllocMemForCode(256);
-
-        m_subhook.Install(reinterpret_cast<void*>(m_addr), wrapper);
-        void* trampoline = m_subhook.GetTrampoline();
-        if (!trampoline)
-            WARN("trampoline is null for 0x%X", m_addr);
-
-        AsmWritter(reinterpret_cast<uintptr_t>(wrapper))
-            .push(reinterpret_cast<int32_t>(trampoline)) // Push default EIP = trampoline
-            .push(asm_regs::esp) // push esp before PUSHA so it can be popped manually after POPA
-            .pusha()             // push general registers
-            .pushf()             // push EFLAGS
-            .add({true, {asm_regs::esp}, offsetof(X86Regs, esp)},
-                 4)              // restore esp value from before pushing the return address
-            .push(asm_regs::esp) // push address of X86Regs struct (handler param)
-            .mov(asm_regs::ecx, reinterpret_cast<uint32_t>(m_this_ptr)) // save this pointer in ECX (thiscall)
-            .call(m_fun_ptr) // call handler (thiscall - calee cleans the stack)
-            .add({true, {asm_regs::esp}, offsetof(X86Regs, esp)}, -4) // make esp value return address aware (again)
-            .mov(asm_regs::eax, {true, {asm_regs::esp}, offsetof(X86Regs, eip)}) // read EIP from X86Regs
-            .mov(asm_regs::ecx, {true, {asm_regs::esp}, offsetof(X86Regs, esp)}) // read esp from X86Regs
-            .mov({true, {asm_regs::ecx}, 0}, asm_regs::eax) // copy EIP to new address of the stack pointer
-            .popf()                                         // pop EFLAGS
-            .popa()                                         // pop general registers. Note: POPA discards esp value
-            .pop(asm_regs::esp)                             // pop esp manually
-            .ret();                                         // return to address read from EIP field in X86Regs
-    }
-
-    void SetAddr(uintptr_t addr)
-    {
-        m_addr = addr;
+        asm_writter
+            .push(asm_regs::esp)               // push address of X86Regs struct (handler param)
+            .call(m_fun_ptr)                   // call handler (cdecl)
+            .add({false, {asm_regs::esp}}, 4); // free handler param
     }
 };
 
 template<typename T>
-class CodeInjection2 : public BaseCodeInjection2
+class CodeInjection2 : public BaseCodeInjection
 {
     T m_functor;
 
 public:
     CodeInjection2(uintptr_t addr, T handler) :
-        BaseCodeInjection2(addr, reinterpret_cast<void*>(&wrapper), this), m_functor(handler)
+        BaseCodeInjection(addr), m_functor(handler)
     {}
 
+private:
     static void __thiscall wrapper(CodeInjection2& self, X86Regs& regs)
     {
         self.m_functor(regs);
+    }
+
+protected:
+    void EmitHandlerCall(AsmWritter& asm_writter) override
+    {
+        asm_writter
+            .push(asm_regs::esp)                                  // push address of X86Regs struct (handler param)
+            .mov(asm_regs::ecx, reinterpret_cast<uint32_t>(this)) // save this pointer in ECX (thiscall)
+            .call(reinterpret_cast<void*>(&wrapper));             // call handler (thiscall - calee cleans the stack)
     }
 };
