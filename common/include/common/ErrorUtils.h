d0ddf4d09920715b9bdb7c3e15f48a92dfdc8b66
Rafał Harabień <rafalh92@outlook.com>
2019-08-15 15:40:42 +0200

    Try to improve error messages from wininet

--- common/include/common/ErrorUtils.h
+++ common/include/common/ErrorUtils.h
@@ -8,8 +8,17 @@ inline std::string get_win32_error_description(DWORD error)
         return "";
 
     LPSTR msg_buf = nullptr;
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                                 nullptr, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&msg_buf, 0, nullptr);
+    int flags = FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+    HMODULE source = nullptr;
+    if (error >= 12000 && error < 13000) {
+        // most likely wininet error
+        source = GetModuleHandleA("wininet.dll");
+        if (source)
+            flags |= FORMAT_MESSAGE_FROM_HMODULE;
+    }
+    size_t size = FormatMessageA(flags, source, error, 0, (LPSTR)&msg_buf, 0, nullptr);
+    if (size == 0)
+        std::fprintf(stderr, "FormatMessageA failed: error %lu\n", GetLastError());
 
     std::string message(msg_buf, size);
 
