98a680328e6ccdbae9318eac31411afa1f1605aa
Rafał Harabień <rafalh92@outlook.com>
2019-12-08 15:26:52 +0100

    Change version to 1.4.2-dev

--- common/include/common/version.h
+++ common/include/common/version.h
@@ -4,7 +4,7 @@
 #define PRODUCT_NAME          "Dash Faction"
 #define VERSION_MAJOR         1
 #define VERSION_MINOR         4
-#define VERSION_PATCH         1
-#define VERSION_STR           "1.4.1"
+#define VERSION_PATCH         2
+#define VERSION_STR           "1.4.2-dev"
 #define PRODUCT_NAME_VERSION  PRODUCT_NAME " " VERSION_STR
 // clang-format on
