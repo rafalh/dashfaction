60d36327dd68176426b78b4afe76f7e73b46b2f0
Rafał Harabień <rafalh92@outlook.com>
2020-06-27 19:01:02 +0200

    Log error when memory allocation fails

--- game_patch/debug/debug.cpp
+++ game_patch/debug/debug.cpp
@@ -1,11 +1,24 @@
 #include "debug_internal.h"
+#include <patch_common/FunHook.h>
+#include <xlog/xlog.h>
 
 #ifndef NDEBUG
 #define DEBUG_PERF
 #endif
 
+FunHook<int(size_t)> callnewh_hook{
+    0x0057A212,
+    [](size_t size) {
+        xlog::error("Failed to allocate %u bytes", size);
+        return callnewh_hook.CallTarget(size);
+    },
+};
+
 void DebugApplyPatches()
 {
+    // Log error when memory allocation fails
+    callnewh_hook.Install();
+
     DebugCmdApplyPatches();
     DebugUnresponsiveApplyPatches();
 #ifdef DEBUG_PERF
