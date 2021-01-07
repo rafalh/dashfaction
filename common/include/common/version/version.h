429a390522fb2e9c6508f93188c698b2a81d4a50
Rafał Harabień <rafalh92@outlook.com>
2021-01-08 00:15:10 +0100

    Change version number to 1.6.1-dev

--- common/include/common/version/version.h
+++ common/include/common/version/version.h
@@ -4,7 +4,7 @@
 #define PRODUCT_NAME          "Dash Faction"
 #define VERSION_MAJOR         1
 #define VERSION_MINOR         6
-#define VERSION_PATCH         0
-#define VERSION_STR           "1.6.0-dev"
+#define VERSION_PATCH         1
+#define VERSION_STR           "1.6.1-dev"
 #define PRODUCT_NAME_VERSION  PRODUCT_NAME " " VERSION_STR
 // clang-format on
