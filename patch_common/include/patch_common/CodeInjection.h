0db84b2365ff77c7deef126150a014ab0660d1a2
Rafał Harabień <rafalh92@outlook.com>
2021-01-19 23:14:28 +0100

    Bring back changes that were unnecessary removed to fix MSVC internal error

--- patch_common/include/patch_common/CodeInjection.h
+++ patch_common/include/patch_common/CodeInjection.h
@@ -26,7 +26,7 @@ public:
         U operator=(U value)
         {
             static_assert(sizeof(T) == sizeof(U));
-            //static_assert(!std::is_null_pointer_v<U>);
+            static_assert(!std::is_null_pointer_v<U>);
             std::memcpy(&this->value, &value, sizeof(T));
             return value;
         }
@@ -35,7 +35,7 @@ public:
         operator U() const
         {
             static_assert(sizeof(T) == sizeof(U));
-            //static_assert(!std::is_null_pointer_v<U>);
+            static_assert(!std::is_null_pointer_v<U>);
             U tmp;
             std::memcpy(&tmp, &this->value, sizeof(T));
             return tmp;
@@ -44,13 +44,23 @@ public:
         template<typename U>
         bool operator==([[ maybe_unused ]] U other) const
         {
-            return static_cast<U>(*this) == other;
+            if constexpr (std::is_null_pointer_v<U>) {
+                return this->value == 0;
+            }
+            else {
+                return static_cast<U>(*this) == other;
+            }
         }
 
         template<typename U>
         bool operator!=([[ maybe_unused ]] U other) const
         {
-            return static_cast<U>(*this) != other;
+            if constexpr (std::is_null_pointer_v<U>) {
+                return this->value != 0;
+            }
+            else {
+                return static_cast<U>(*this) != other;
+            }
         }
 
         template<typename U>
