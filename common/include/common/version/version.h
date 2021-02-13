cbef11fc80a9d152acd07799dc00d5e7607111eb
Rafał Harabień <rafalh92@outlook.com>
2021-02-13 20:13:03 +0100

    Add VERSION_TYPE_REVISION macro

--- common/include/common/version/version.h
+++ common/include/common/version/version.h
@@ -10,23 +10,27 @@
 #define VERSION_TYPE_DEV      0
 #define VERSION_TYPE_ALPHA    1
 #define VERSION_TYPE_BETA     2
-#define VERSION_TYPE_RELEASE  3
+#define VERSION_TYPE_RC       3
+#define VERSION_TYPE_RELEASE  4
 
 // Variables to be modified during the release process
-#define PRODUCT_NAME   "Dash Faction"
-#define VERSION_MAJOR  1
-#define VERSION_MINOR  6
-#define VERSION_PATCH  1
-#define VERSION_TYPE   VERSION_TYPE_DEV
+#define PRODUCT_NAME           "Dash Faction"
+#define VERSION_MAJOR          1
+#define VERSION_MINOR          6
+#define VERSION_PATCH          1
+#define VERSION_TYPE           VERSION_TYPE_DEV
+#define VERSION_TYPE_REVISION  1
 
 // clang-format on
 
 #if VERSION_TYPE == VERSION_TYPE_DEV
 #define VERSION_SUFFIX        "-dev"
 #elif VERSION_TYPE == VERSION_TYPE_ALPHA
-#define VERSION_SUFFIX        "-alpha"
+#define VERSION_SUFFIX        "-alpha" TOSTRING(VERSION_TYPE_REVISION)
 #elif VERSION_TYPE == VERSION_TYPE_BETA
-#define VERSION_SUFFIX        "-beta"
+#define VERSION_SUFFIX        "-beta" TOSTRING(VERSION_TYPE_REVISION)
+#elif VERSION_TYPE == VERSION_TYPE_RC
+#define VERSION_SUFFIX        "-rc" TOSTRING(VERSION_TYPE_REVISION)
 #elif VERSION_TYPE == VERSION_TYPE_RELEASE
 #define VERSION_SUFFIX        ""
 #else
