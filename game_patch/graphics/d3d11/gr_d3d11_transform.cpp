ff90c14d8c03ecd79f4f22977725888355b8aea5
Rafał Harabień <rafalh92@outlook.com>
2024-05-31 22:33:31 +0200

    d3d11: fix standard projection matrix definition
    GpuMatrix4x4 is column-major, so rows are actually columns.

--- game_patch/graphics/d3d11/gr_d3d11_transform.cpp
+++ game_patch/graphics/d3d11/gr_d3d11_transform.cpp
@@ -11,12 +11,13 @@ namespace df::gr::d3d11
 {
     GpuMatrix4x4 build_proj_matrix()
     {
-        // FIXME: this projection matrix is non-standard. See:
+        // TODO: use variable fov and far plane
+    #if 0
+        // Standard D3D projection matrix, 1/z, maps near plane to 0 and far plane to 1. See:
         // https://docs.microsoft.com/en-us/windows/win32/dxtecharts/the-direct3d-transformation-pipeline
         // https://docs.microsoft.com/en-us/windows/win32/direct3d9/projection-transform
-    #if 0 // standard d3d projection matrix
-        float z_near = 1.0f;
-        float z_far = 1000.0f;
+        float z_near = 0.15f;
+        float z_far = 275.0f;
         float q = z_far / (z_far - z_near);
         float fov_w = 3.14592 / 180.0f * 120.0f;
         float fov_h = fov_w * 3.0f / 4.0f;
@@ -25,10 +26,12 @@ namespace df::gr::d3d11
         return {{
             {w, 0.0f, 0.0f, 0.0f},
             {0.0f, h, 0.0f, 0.0f},
-            {0.0f, 0.0f, q, 1.0f},
-            {0.0f, 0.0f, -q * z_near, 0.0f},
+            {0.0f, 0.0f, q, -q * z_near},
+            {0.0f, 0.0f, 1.0f, 0.0f},
         }};
-    #else // rf projection, normalized _34 element
+    #else
+        // RF projection, 1/z, maps near plane to 1 and far plane to 0
+        // It has some benefits, see "reversed-Z trick" in https://developer.nvidia.com/content/depth-precision-visualized
         using rf::gr::matrix_scale;
         using rf::gr::d3d::zm;
         return {{
