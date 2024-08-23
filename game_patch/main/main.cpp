rafalh/dashfaction

commit 9bcfa83cf1fbd890a8eca9ff9b0bd263571dee7e

Do not load unnecessary VPPs in dedicated server mode
Following VPPs will no longer be loaded:
- audio.vpp
- maps_*.vpp
- levels[123].vpp
- maps[1234].vpp
- music.vpp
- ui.vpp
- dashfaction.vpp

Also don't check if sound files exist in dedicated server mode.
This change may slightly speed up server startup and reduce number of
logged errors in minimal setups.

game_patch/main/main.cpp
@@ -123,13 +123,15 @@ CodeInjection after_level_render_hook{
 CodeInjection after_frame_render_hook{
     0x004B2DC2,
     []() {
-        // Draw on top (after scene)
-        frametime_render_ui();
-        multi_render_level_download_progress();
+        if (!rf::is_dedicated_server) {
+            // Draw on top (after scene)
+            frametime_render_ui();
+            multi_render_level_download_progress();
 #if !defined(NDEBUG) && defined(HAS_EXPERIMENTAL)
-        experimental_render();
+            experimental_render();
 #endif
-        debug_render_ui();
+            debug_render_ui();
+        }
     },
 };
 

game_patch/misc/vpackfile.cpp
@@ -18,6 +18,7 @@
 #include "../rf/file/file.h"
 #include "../rf/file/packfile.h"
 #include "../rf/crt.h"
+#include "../rf/multi.h"
 #include "../os/console.h"
 
 #define CHECK_PACKFILE_CHECKSUM 0 // slow (1 second on SSD on first load after boot)
@@ -443,22 +444,26 @@ static void vpackfile_init_new()
     g_loopup_table.reserve(10000);
 
     if (get_installed_game_lang() == LANG_GR) {
-        rf::vpackfile_add("audiog.vpp", nullptr);
-        rf::vpackfile_add("maps_gr.vpp", nullptr);
-        rf::vpackfile_add("levels1g.vpp", nullptr);
-        rf::vpackfile_add("levels2g.vpp", nullptr);
-        rf::vpackfile_add("levels3g.vpp", nullptr);
+        if (!rf::is_dedicated_server) {
+            rf::vpackfile_add("audiog.vpp", nullptr);
+            rf::vpackfile_add("maps_gr.vpp", nullptr);
+            rf::vpackfile_add("levels1g.vpp", nullptr);
+            rf::vpackfile_add("levels2g.vpp", nullptr);
+            rf::vpackfile_add("levels3g.vpp", nullptr);
+        }
         rf::vpackfile_add("ltables.vpp", nullptr);
     }
     else if (get_installed_game_lang() == LANG_FR) {
-        rf::vpackfile_add("audiof.vpp", nullptr);
-        rf::vpackfile_add("maps_fr.vpp", nullptr);
-        rf::vpackfile_add("levels1f.vpp", nullptr);
-        rf::vpackfile_add("levels2f.vpp", nullptr);
-        rf::vpackfile_add("levels3f.vpp", nullptr);
+        if (!rf::is_dedicated_server) {
+            rf::vpackfile_add("audiof.vpp", nullptr);
+            rf::vpackfile_add("maps_fr.vpp", nullptr);
+            rf::vpackfile_add("levels1f.vpp", nullptr);
+            rf::vpackfile_add("levels2f.vpp", nullptr);
+            rf::vpackfile_add("levels3f.vpp", nullptr);
+        }
         rf::vpackfile_add("ltables.vpp", nullptr);
     }
-    else {
+    else if (!rf::is_dedicated_server) {
         rf::vpackfile_add("audio.vpp", nullptr);
         rf::vpackfile_add("maps_en.vpp", nullptr);
         rf::vpackfile_add("levels1.vpp", nullptr);
@@ -468,15 +473,19 @@ static void vpackfile_init_new()
     rf::vpackfile_add("levelsm.vpp", nullptr);
     // rf::vpackfile_add("levelseb.vpp", nullptr);
     // rf::vpackfile_add("levelsbg.vpp", nullptr);
-    rf::vpackfile_add("maps1.vpp", nullptr);
-    rf::vpackfile_add("maps2.vpp", nullptr);
-    rf::vpackfile_add("maps3.vpp", nullptr);
-    rf::vpackfile_add("maps4.vpp", nullptr);
+    if (!rf::is_dedicated_server) {
+        rf::vpackfile_add("maps1.vpp", nullptr);
+        rf::vpackfile_add("maps2.vpp", nullptr);
+        rf::vpackfile_add("maps3.vpp", nullptr);
+        rf::vpackfile_add("maps4.vpp", nullptr);
+    }
     rf::vpackfile_add("meshes.vpp", nullptr);
     rf::vpackfile_add("motions.vpp", nullptr);
-    rf::vpackfile_add("music.vpp", nullptr);
-    rf::vpackfile_add("ui.vpp", nullptr);
-    load_dashfaction_vpp();
+    if (!rf::is_dedicated_server) {
+        rf::vpackfile_add("music.vpp", nullptr);
+        rf::vpackfile_add("ui.vpp", nullptr);
+        load_dashfaction_vpp();
+    }
     rf::vpackfile_add("tables.vpp", nullptr);
     addr_as_ref<int>(0x01BDB218) = 1;          // VPackfilesLoaded
     addr_as_ref<uint32_t>(0x01BDB210) = 10000; // NumFilesInVfs
@@ -499,8 +508,10 @@ static void vpackfile_init_new()
         write_mem_ptr(0x004B082B + 1, "localized_strings.tbl");
     }
 
-    // Allow modded strings.tbl in ui.vpp
-    force_file_from_packfile("strings.tbl", "ui.vpp");
+    if (!rf::is_dedicated_server) {
+        // Allow modded strings.tbl in ui.vpp
+        force_file_from_packfile("strings.tbl", "ui.vpp");
+    }
 
     xlog::info("Packfiles initialization took %lums", GetTickCount() - start_ticks);
     xlog::info("Packfile name collisions: %d", g_num_name_collisions);

game_patch/sound/sound.cpp
@@ -430,6 +430,16 @@ FunHook<bool(const char*)> snd_pc_file_exists_hook{
     }
 };
 
+FunHook<int(const char*, float, float, float)> snd_get_handle_hook{
+    0x005054B0,
+    [](const char* filename, float min_range, float base_volume, float rolloff) {
+        if (!rf::sound_enabled) {
+            return -1;
+        }
+        return snd_get_handle_hook.call_target(filename, min_range, base_volume, rolloff);
+    },
+};
+
 void snd_ds_apply_patch();
 
 void apply_sound_patches()
@@ -483,6 +493,9 @@ void apply_sound_patches()
 
     // Log when sound file cannot be found
     snd_pc_file_exists_hook.install();
+
+    // Do not update sounds array in dedicated server mode
+    snd_get_handle_hook.install();
 }
 
 void register_sound_commands()

commit 870f869353eecb0e0a3a1e15c8ac6e10f08af892

Fix level download regression in dedicated server
Introduced in 9bcfa83cf1fbd890a8eca9ff9b0bd263571dee7e
Fixes #252

game_patch/main/main.cpp
@@ -106,6 +106,7 @@ FunHook<int()> rf_do_frame_hook{
         int result = rf_do_frame_hook.call_target();
         maybe_autosave();
         debug_do_frame_post();
+        multi_level_download_update();
         return result;
     },
 };
@@ -126,7 +127,6 @@ CodeInjection after_frame_render_hook{
         if (!rf::is_dedicated_server) {
             // Draw on top (after scene)
             frametime_render_ui();
-            multi_render_level_download_progress();
 #if !defined(NDEBUG) && defined(HAS_EXPERIMENTAL)
             experimental_render();
 #endif

game_patch/multi/level_download.cpp
@@ -694,7 +694,7 @@ void level_download_init()
     download_level_force_cmd.register_cmd();
 }
 
-void multi_render_level_download_progress()
+void multi_level_download_update()
 {
     LevelDownloadManager::instance().process();
 }

game_patch/multi/multi.h
@@ -78,7 +78,7 @@ struct DashFactionServerInfo
     std::optional<float> max_fov;
 };
 
-void multi_render_level_download_progress();
+void multi_level_download_update();
 void multi_do_patch();
 void multi_after_full_game_init();
 void multi_init_player(rf::Player* player);