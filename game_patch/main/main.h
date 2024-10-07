5a17790766b1bc6fb8c9d4bfade53357f42cf51f
Goober <gooberCP@gmail.com>
2024-10-07 11:58:03 -0230

    use g_ convention

--- game_patch/main/main.h
+++ game_patch/main/main.h
@@ -6,7 +6,6 @@ extern GameConfig g_game_config;
 
 // random number generator
 extern std::mt19937 rng;
-extern std::uniform_int_distribution<std::size_t> dist;
 void initialize_random_generator();
 
 #ifdef _WINDOWS_
