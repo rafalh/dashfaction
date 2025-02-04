5e90446ec22613ed3edad1e0652a1e63f8c527fd
is-this-c <87069698+is-this-c@users.noreply.github.com>
2025-02-04 16:48:07 +1300

    Update multi.{cpp,h}

--- game_patch/multi/multi.cpp
+++ game_patch/multi/multi.cpp
@@ -336,7 +336,7 @@ void multi_init_player(rf::Player* player)
     multi_kill_init_player(player);
 }
 
-std::string_view multi_game_type_name(const NetGameType game_type) {
+std::string_view multi_game_type_name(const rf::NetGameType game_type) {
     if (game_type == NG_TYPE_DM) {
         return std::string_view{rf::strings::deathmatch};
     } else if (game_type == NG_TYPE_CTF) {
