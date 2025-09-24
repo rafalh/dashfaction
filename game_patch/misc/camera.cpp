66e3f0fba5241b2f8860e0410617e31ca67591e3
is-this-c <87069698+is-this-c@users.noreply.github.com>
2025-09-25 01:09:22 +1200

    Update camera.cpp

--- game_patch/misc/camera.cpp
+++ game_patch/misc/camera.cpp
@@ -20,6 +20,34 @@ FunHook<void(rf::Camera*)> camera_update_shake_hook{
     },
 };
 
+CallHook<void(rf::Camera*, float, float)> entity_fire_primary_weapon_camera_shake_hook{
+    0x00426C79,
+    [] (rf::Camera* const cp, const float amplitude, const float time_seconds) {
+        const bool server_force_weapon_shake = rf::is_multi
+            && !rf::is_server
+            && get_af_server_info()
+            && !get_af_server_info()->allow_no_ss;
+        if (g_game_config.weapon_shake || server_force_weapon_shake) {
+            entity_fire_primary_weapon_camera_shake_hook.call_target(cp, amplitude, time_seconds);
+        }
+    },
+};
+
+ConsoleCommand2 weapon_shake_cmd{
+    "weapon_shake",
+    [] {
+        g_game_config.weapon_shake = !g_game_config.weapon_shake;
+        g_game_config.save();
+        rf::console::print(
+            "Weapon shake is {}",
+            g_game_config.weapon_shake
+                ? "enabled"
+                : "disabled [multiplayer servers may force weapon shake]"
+        );
+    },
+    "Toggle camera shake from weapon fire [mutiplayer servers may force weapon shake]",
+};
+
 void camera_do_patch()
 {
     // Fix crash when executing camera2 command in main menu
