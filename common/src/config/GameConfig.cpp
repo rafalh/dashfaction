f8aedec8636ad828190c1698a014c47ccfcd403a
is-this-c <87069698+is-this-c@users.noreply.github.com>
2025-09-25 01:10:40 +1200

    Update GameConfig.cpp

--- common/src/config/GameConfig.cpp
+++ common/src/config/GameConfig.cpp
@@ -175,6 +175,7 @@ bool GameConfig::visit_vars(T&& visitor, bool is_save)
     result &= visitor(dash_faction_key, "Glares", glares);
     result &= visitor(dash_faction_key, "Linear Pitch", linear_pitch);
     result &= visitor(dash_faction_key, "Show Enemy Bullets", show_enemy_bullets);
+    result &= visitor(dash_faction_key, "Weapon Shake", weapon_shake);
     result &= visitor(dash_faction_key, "Keep Launcher Open", keep_launcher_open);
     result &= visitor(dash_faction_key, "Skip Cutscene Control", skip_cutscene_ctrl);
     result &= visitor(dash_faction_key, "Damage Screen Flash", damage_screen_flash);
